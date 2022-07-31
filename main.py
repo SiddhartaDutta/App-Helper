a28b77c033085c552ff1de03e1a8f8f15fad82c2
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-07-31 16:59:55 -0700

    Added audio string splice

---
+++ main.py
@@ -0,0 +1,36 @@
+import speech_recognition as sr
+import os
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Speak anything: ')
+    audio = recognizer.listen(source)
+
+    try:
+
+        # Convert audio to string
+        text = recognizer.recognize_google(audio)
+        print('Original Read: {}'.format(text))
+
+        # Modify string to just open command
+        appIndex = text.find('open')
+        text = text[appIndex:]
+
+        print('Spliced Read: {}'.format(text))
+
+        try:
+            pathToProgramFiles = "C:\Program Files (x86)"
+
+            test = os.getcwd()
+            print(test)
+
+            path = "C:\Users\siddh\OneDrive\Documents\Visual Studio Code"
+            os.chdir(path)
+            test = os.getcwd()
+            print(test)
+        except:
+            print('yur')
+
+    except:
+        print('Not understood')
